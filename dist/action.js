bdd4c71f52b3e703dc550ade3b8d285e75f6427a
zyrouge <naveencnaveen96@gmail.com>
2022-07-16 21:35:40 +0530

    chore: push correct build files

--- dist/action.js
+++ dist/action.js
@@ -50,15 +50,21 @@ const action = (options) => __awaiter(void 0, void 0, void 0, function* () {
     const git = new git_1.Git({
         directory: temporaryDirectory,
     });
-    // git clone <url> <path>
-    git.print("clone", yield git.run(["clone", ghRepoUrl, "."]));
-    log_1.logger.info("git", `Cloned to ${temporaryDirectory}`);
     // git config --local user.name <username>
     git.print("config: user.name", yield git.run(["config", "--local", "user.name", options.localUsername]));
     log_1.logger.debug("git", `Changed local username to ${options.localUsername}`);
     // git config --local user.email <email>
     git.print("config: user.email", yield git.run(["config", "--local", "user.email", options.localEmail]));
     log_1.logger.debug("git", `Changed local email to ${options.localEmail}`);
+    // git init
+    git.print("init", yield git.run(["init"]));
+    log_1.logger.info("git", `Initialized repository at ${temporaryDirectory}`);
+    // git remote add origin <url>
+    git.print("add-remote", yield git.run(["remote", "add", "origin", ghRepoUrl]));
+    log_1.logger.info("git", `Added remote "origin" -> ${ghRepoUrl}`);
+    // git fetch
+    git.print("fetch", yield git.run(["fetch"]));
+    log_1.logger.info("git", "Fetch repository");
     // git show-branch remotes/origin/<remote-branch-name>
     const checkBranch = yield git
         .run(["show-branch", `remotes/origin/${options.branch}`])
